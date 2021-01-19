9cf989959be695350b072064ab698d2efc8787b4
Paul Realpe <paulmrg461@gmail.com>
2021-01-19 10:34:09 -0500

    10. HomePage basic interface

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,15 +1,60 @@
+import 'package:brand_names/src/models/band.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Band> bands = [
+    Band(id: '1', name: 'Metallica', votes: 5),
+    Band(id: '2', name: 'Guns and Roses', votes: 7),
+    Band(id: '3', name: 'Aerosmith', votes: 6),
+    Band(id: '4', name: 'Queen', votes: 4),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Center(
+          child: Text(
+            'Band Names',
+            style: TextStyle(
+              color: Colors.black87,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.white,
       ),
-      body: Center(
-        child: Text('Hello World!'),
+      body: ListView.builder(
+        itemCount: bands.length,
+        itemBuilder: (context, i) => _bandTile(bands[i]),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.deepPurple,
+      ),
+    );
+  }
+
+  ListTile _bandTile(Band band) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text(
+          band.name.substring(0, 2),
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        backgroundColor: Colors.deepPurple,
+      ),
+      title: Text(band.name),
+      trailing: Text(
+        '${band.votes}',
+        style: TextStyle(fontSize: 18),
+      ),
+      onTap: () => print(band.name),
     );
   }
 }
