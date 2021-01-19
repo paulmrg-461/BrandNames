e826715ea7e78403f3c509204bf3219e5069ad2b
Paul Realpe <paulmrg461@gmail.com>
2021-01-19 11:26:15 -0500

    11. Add to list - InputDialog, CupertinoDialog

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,8 @@
-import 'package:brand_names/src/models/band.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:brand_names/src/models/band.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -33,7 +36,7 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, i) => _bandTile(bands[i]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: addNewBand,
         child: Icon(Icons.add),
         backgroundColor: Colors.deepPurple,
       ),
@@ -56,5 +59,63 @@ class _HomePageState extends State<HomePage> {
       ),
       onTap: () => print(band.name),
     );
+  }
+
+  addNewBand() {
+    final textController = new TextEditingController();
+
+    Platform.isIOS
+        ? iOsAlertDialog(textController)
+        : androidAlertDialog(textController);
+  }
+
+  void addBandToList(String name) {
+    if (name.length > 1) {
+      this
+          .bands
+          .add(new Band(id: DateTime.now().toString(), name: name, votes: 0));
+      setState(() {});
+    }
+    Navigator.pop(context);
+  }
+
+  androidAlertDialog(TextEditingController textController) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('New band name:'),
+              content: TextField(
+                controller: textController,
+              ),
+              actions: <Widget>[
+                MaterialButton(
+                  onPressed: () => addBandToList(textController.text),
+                  child: Text('Add'),
+                  textColor: Colors.deepPurple,
+                  elevation: 5,
+                )
+              ],
+            ));
+  }
+
+  iOsAlertDialog(TextEditingController textController) {
+    showCupertinoDialog(
+        context: context,
+        builder: (_) => CupertinoAlertDialog(
+              title: Text('New band name:'),
+              content: CupertinoTextField(
+                controller: textController,
+              ),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                    isDestructiveAction: true,
+                    child: Text('Dismiss'),
+                    onPressed: () => Navigator.pop(context)),
+                CupertinoDialogAction(
+                    isDefaultAction: true,
+                    child: Text('Add'),
+                    onPressed: () => addBandToList(textController.text)),
+              ],
+            ));
   }
 }
